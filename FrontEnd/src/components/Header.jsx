aba9f712efb46704f79e54d9c03555972e1f226a
Dylan Aaron <116681352+daaron678@users.noreply.github.com>
2025-08-02 00:02:50 -0400

    Updated about Sortflix link

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
       <header className="bg-white shadow p-4 flex justify-between items-center">
-        <a href="#" className="text-sm text-blue-600 underline items-center">About SortFlix</a>
+        <a href="https://github.com/daaron678/SortFlix" className="text-sm text-blue-600 underline items-center">About SortFlix</a>
         <h1 className="text-3xl font-bold text-orange-400">SortFlix</h1>
         <div />
       </header>
