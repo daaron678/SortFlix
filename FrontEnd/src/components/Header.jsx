88ad4069e38482a21a1d9b593aa6dcbeaed6e682
Your Name <dylan.cc.aaron@gmail.com>
2025-08-04 13:28:10 -0400

    emptied

--- FrontEnd/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (
-      <header className="bg-white shadow p-4 flex justify-between items-center">
-        <a href="#" className="text-sm text-blue-600 underline items-center">About SortFlix</a>
-        <h1 className="text-3xl font-bold text-orange-400">SortFlix</h1>
-        <div />
-      </header>
-    );
-  }
